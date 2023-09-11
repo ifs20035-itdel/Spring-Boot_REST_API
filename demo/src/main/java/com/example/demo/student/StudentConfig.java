e1120ce2cf4d1acd04fe5d5bfac6787da8d6b53c
ifs20035-itdel <ifs20035@students.del.ac.id>
2023-09-11 23:11:50 +0700

    add config data

--- demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -4,13 +4,38 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @Configuration
 public class StudentConfig {
 
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
+            Student michael = new Student(
+                    "Michael",
+                    "asm34686@gmail.com",
+                    21,
+                    LocalDate.of(2002, Month.APRIL, 23)
+            );
+            Student albert = new Student(
+                    "Albert",
+                    "forcealbert@gmail.com",
+                    21,
+                    LocalDate.of(2002, Month.APRIL, 23)
+            );
+            Student sholay = new Student(
+                    "Sholay",
+                    "napitupulumichael@outlook.com",
+                    21,
+                    LocalDate.of(2002, Month.APRIL, 23)
+            );
 
+            repository.saveAll(
+                    List.of(michael, albert, sholay)
+            );
         };
     }
 }
