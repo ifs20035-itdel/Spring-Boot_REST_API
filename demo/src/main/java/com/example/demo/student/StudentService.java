b6a1fac8349f736abcc5d3e991bc786b307f5162
ifs20035-itdel <ifs20035@students.del.ac.id>
2023-09-13 20:23:43 +0700

    Create PUT method for updating email or/and name

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -9,6 +9,7 @@ import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -43,8 +44,33 @@ public class StudentService {
         studentRepository.deleteById(studentId);
     }
 
-    public Student editStudent(Long studentId, String name, String email) {
-        Student exist = studentRepository.existsById(studentId);
+    public void editStudent(
+            Long studentId,
+            String name,
+            String email
+    ) {
+        Student student = studentRepository
+                .findById(studentId).orElseThrow(() -> new IllegalStateException(
+                        "Student with Id: " + studentId + " doesn't exists"
+                ));
+
+        if (name != null &&
+                name.length() > 0 &&
+                !Objects.equals(student.getName(), name)
+        ) {
+            student.setName(name);
+        }
+
+        if (email != null &&
+                email.length()>0 &&
+                !Objects.equals(student.getEmail(), email)
+        ) {
+           Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+           if(studentOptional.isPresent()){
+               throw new IllegalStateException("email taken");
+           }
+           student.setEmail(email);
+        }
 
     }
 }
